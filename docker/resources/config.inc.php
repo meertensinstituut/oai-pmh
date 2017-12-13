a8ac813a2b7fd04e47f3f907536608095d228ced
Matthijs Brouwer <matthijs@brouwer.info>
2017-12-13 15:42:09 +0100

    dockerfile

---
+++ docker/resources/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$dataProvider = "Demo";
+
+$identifyResponseName = "Demo";
+$identifyAdminEmail = "noreply@demo.dev";
+
+$mysqlDatabase = "oai-pmh";
+$mysqlHost = "127.0.0.1";
+$mysqlUsername = "demo";
+$mysqlPassword = "demo";
--- docker/resources/mariadb_install.sql
+++ docker/resources/mariadb_install.sql
@@ -1,3 +1,3 @@
 CREATE USER 'demo'@'%' IDENTIFIED BY 'demo';
-GRANT ALL PRIVILEGES ON `oai-pmh` . * TO 'demo'@'%';
 CREATE DATABASE `oai-pmh`;
+GRANT ALL PRIVILEGES ON `oai-pmh` . * TO 'demo'@'%';
