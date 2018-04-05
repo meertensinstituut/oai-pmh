6ce0b485b7eee222a135f003cd4b33c50fded9e6
Vic Ding <qiqing.ding@meertens.knaw.nl>
2018-04-05 11:38:42 +0200

    switch to dev branch, change structure

--- src/config/config_demo_nederlab.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dataProvider = "Nederlab";
-
-$identifyResponseName = "Nederlab";
-$identifyAdminEmail = "noreply@nederlab.dev";
-
-$brokerUrl = "http://www.nederlab.dev/broker/search/";
-$brokerKey = null;
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker kill $(docker ps -q)
+docker container prune -f
